20731aa62fa1c1b68dbf11892788a567d7cf9e25
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-09-30 19:13:26 +0530

    added stone paper scissor logic to the game

--- stone_paper_scissors/scripts/script.js
+++ stone_paper_scissors/scripts/script.js
@@ -2,8 +2,11 @@ console.log("STONE PAPER SCISSORS");
 
 // UI
 const welcomeWrapper = document.querySelector(".welcome-wrapper");
+const finalStatement = document.querySelector(".final-statement");
+
 // INPUTS
 const playerNameInput = document.querySelector("#player-name");
+
 // BUTTONS
 const submitButton = document.querySelector(".submit-button");
 const stoneButton = document.querySelector("#stone-button");
@@ -11,15 +14,17 @@ const paperButton = document.querySelector("#paper-button");
 const scissorsButton = document.querySelector("#scissors-button");
 const computerChoiceButton = document.querySelector("#computer-choice-button");
 const nameTitle = document.querySelector(".name-title");
+const submitChoiceButton = document.querySelector(".submit-choice-button");
+
 let username = "";
 let userChoice = "";
-let computerChoice = "";
 let showInput = true;
+let canChoose = true;
 const choicePool = ["STONE", "PAPER", "SCISSORS"];
+let gameOver = false;
 
 const checkShowInputVisiblity = () => {
   if (!showInput) {
-    // welcomeWrapper.classList.remove("welcome-wrapper");
     welcomeWrapper.classList.add("hidden");
     nameTitle.textContent = username;
   } else {
@@ -29,7 +34,6 @@ const checkShowInputVisiblity = () => {
 
 // player name event
 playerNameInput.addEventListener("input", (e) => {
-  // console.log("USERNAME: ", username);
   username = e.target.value;
 });
 
@@ -38,4 +42,47 @@ submitButton.addEventListener("click", () => {
   playerNameInput.value = "";
   showInput = false;
   checkShowInputVisiblity();
+});
+
+submitChoiceButton.addEventListener("click", () => {
+  if (!canChoose) {
+    const computerChoiceFinal =
+      choicePool[Math.floor(Math.random() * choicePool.length)];
+
+    if (computerChoiceFinal === userChoice) {
+      finalStatement.textContent = "It's a draw";
+    } else if (
+      (userChoice === "STONE" && computerChoiceFinal === "SCISSORS") ||
+      (userChoice === "PAPER" && computerChoiceFinal === "STONE") ||
+      (userChoice === "SCISSORS" && computerChoiceFinal === "PAPER")
+    ) {
+      finalStatement.textContent = "You WON";
+    } else {
+      finalStatement.textContent = "You LOSE";
+    }
+
+    computerChoiceButton.textContent = computerChoiceFinal;
+    canChoose = true;
+  }
+});
+
+stoneButton.addEventListener("click", () => {
+  if (canChoose) {
+    userChoice = "STONE";
+    canChoose = false;
+  }
+});
+
+paperButton.addEventListener("click", () => {
+  if (canChoose) {
+    userChoice = "PAPER";
+    canChoose = false;
+  }
+});
+
+scissorsButton.addEventListener("click", () => {
+  if (canChoose) {
+    userChoice = "SCISSORS";
+    canChoose = false;
+  }
 });
